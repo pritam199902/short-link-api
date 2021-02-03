582a6e3be989612eefe3fa5e3a87e3f8641a30dc
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2021-02-04 01:56:07 +0530

    date added to sch

--- model.js
+++ model.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const urlSchema = mongoose.Schema({
     orginalUrl : String,
     shortUrl : String,
-    clicks : {type: Number, default: 0}
+    clicks : {type: Number, default: 0},
+    time : {
+        type : Date,
+        default: Date.now()
+    }
 })
 
 const UrlData = mongoose.model('UrlData', urlSchema)
